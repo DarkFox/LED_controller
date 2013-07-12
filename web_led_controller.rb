7e9767a1e8d9625f5bbc0b7ba94cb98c1b8d8c25
Martin Troels Eberhardt <martin@darkfox.dk>
2013-07-12 13:00:23 +0200

    Put code into a class.

--- web_led_controller.rb
+++ web_led_controller.rb
@@ -1,60 +1,62 @@
 require 'rubygems'
-require 'sinatra'
+require "sinatra/base"
 require './led_controller.rb'
 
 CONTROLLER = LedController.new("/dev/tty.usbmodemfd121")
 
-get '/' do
-  erb :'index.html'
-end
-
-get '/state' do
-  CONTROLLER.current_state.to_json
-end
-
-get '/led' do
-
-  # Modes
-  # 0: OFF
-  # 1: ON
-  # 2: Pulse
-  # 3: Random fade
-  # 4: Rainbow fade
-  # 5: Strobe
-  # 6: Sleep
-  # 7: Direct
-  if params[:mode]
-    CONTROLLER.set_mode params[:mode]
-    'OK'
+class WebLedController < Sinatra::Base
+  get '/' do
+    erb :'index.html'
   end
 
-  # Hue
-  # 0-360
-  if params[:hue]
-    CONTROLLER.set_hue params[:hue]
-    'OK'
+  get '/state' do
+    CONTROLLER.current_state.to_json
   end
 
-  # Saturation
-  # 0-255
-  if params[:sat]
-    CONTROLLER.set_sat params[:sat]
-    'OK'
-  end
+  get '/led' do
 
-  # Luminosity
-  # 0-255
-  if params[:lum]
-    CONTROLLER.set_lum params[:lum]
-    'OK'
-  end
+    # Modes
+    # 0: OFF
+    # 1: ON
+    # 2: Pulse
+    # 3: Random fade
+    # 4: Rainbow fade
+    # 5: Strobe
+    # 6: Sleep
+    # 7: Direct
+    if params[:mode]
+      CONTROLLER.set_mode params[:mode]
+      'OK'
+    end
 
-  # Interval
-  # 0-2048
-  if params[:interval]
-    CONTROLLER.set_interval params[:interval]
-    'OK'
-  end
+    # Hue
+    # 0-360
+    if params[:hue]
+      CONTROLLER.set_hue params[:hue]
+      'OK'
+    end
 
-  CONTROLLER.save
+    # Saturation
+    # 0-255
+    if params[:sat]
+      CONTROLLER.set_sat params[:sat]
+      'OK'
+    end
+
+    # Luminosity
+    # 0-255
+    if params[:lum]
+      CONTROLLER.set_lum params[:lum]
+      'OK'
+    end
+
+    # Interval
+    # 0-2048
+    if params[:interval]
+      CONTROLLER.set_interval params[:interval]
+      'OK'
+    end
+
+    CONTROLLER.save
+  end
 end
